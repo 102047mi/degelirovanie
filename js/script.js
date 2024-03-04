ef47cd75d4ff27be39644b18089be84ef5ecb934
Likoy <122590358+102047mi@users.noreply.github.com>
2024-03-04 16:39:42 +0500

    Шапка с вкладками
    Шапка с вкладками

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const tabList = document.querySelector('#tabList');
+const tabcontent = document.querySelectorAll('.tabcontent');
+const tabLinks = document.querySelectorAll('.tablink');
+
+const defaultOpen = document.querySelector('#defaultOpen');
+const active = document.querySelector('.tabcontent.active');
+
+// getComputedStyle(Element)- позволяет получить значения любого
+// CSS свойства элемента даже из CSS файла
+
+console.log(getComputedStyle(active).backgroundColor)
+
+//установить для кнопки по умолчанию такой же цвет как и у активной вкладки
+defaultOpen.style.backgroundColor = getComputedStyle(active).backgroundColor;
+
+tabList.addEventListener('click', (e) => {
+    //Кнопка по которой нажали
+    const targetElement = e.target;
+    console.log(targetElement);;
+    //элемент.dataset.имя - Обращение к свойству data фтрибуту с указаным именем
+    const dataCity = targetElement.dataset.city;
+    console.log(dataCity)
+
+    //Перебрать все вкладки с контентом
+    for (let tab of tabcontent) {
+        //скрыть все вкладки
+        tab.classList.remove('active');
+    }
+})
